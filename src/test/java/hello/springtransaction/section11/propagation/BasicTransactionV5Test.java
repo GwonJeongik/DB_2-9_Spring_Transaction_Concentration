97dc8108d990799cd0ea9ae45fa918dba3177e8c
ik <jeongik11029@naver.com>
2024-09-08 20:39:09 +0900

    트랜잭션 전파 -> 외부 트랜잭션 시작 -> 내부 트랜잭션 시작 -> 내부 트랜잭션 롤백 -> rollbackOnly = true로 설정 -> 외부 트랜잭션 커밋 -> rollbackOnly = true이므로 -> 물리 트랜잭션 롤백

--- src/test/java/hello/springtransaction/section11/propagation/BasicTransactionV5Test.java
+++ src/test/java/hello/springtransaction/section11/propagation/BasicTransactionV5Test.java
@@ -1,6 +1,7 @@
 package hello.springtransaction.section11.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,16 +10,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
 
 /**
- * 5. 스프링 트랜잭션 전파5 - 외부 롤백
- *
+ * 6. 스프링 트랜잭션 전파5 - 내부 롤백
+ * <p>
  * 외부 트랜잭션 시작하고, 내부 트랜잭션 시작
- * 내부 트랜잭션 커밋 [논리 트랜잭션]
- * 외부 트랜잭션 롤백 [논리 트랜잭션], [외부 트랜잭션만 물리적인 커밋, 롤백을 처리]
+ * 내부 트랜잭션 롤백 [논리 트랜잭션]
+ * `rollbackOnly = true`로 설정됨.
+ * 외부 트랜잭션 커밋 [논리 트랜잭션], [외부 트랜잭션만 물리적인 커밋, 롤백을 처리]
  * 물리 트랜잭션 롤백
  */
 @Slf4j
@@ -45,10 +48,11 @@ public class BasicTransactionV5Test {
 
         log.info("내부 트랜잭션 시작");
         TransactionStatus inner = transactionManager.getTransaction(new DefaultTransactionAttribute());
-        log.info("내부 트랜잭션 커밋");
-        transactionManager.commit(inner);
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner); // `rollbackOnly = true`로 설정
 
-        log.info("외부 트랜잭션 롤백");
-        transactionManager.rollback(outer);
+        log.info("외부 트랜잭션 커밋");
+        Assertions.assertThatThrownBy(() -> transactionManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
     }
 }
